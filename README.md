0a25d802efd8e9b9a760d9ef0b9f5584870f4947
Dave Timmins <davetimmins@outlook.com>
2016-09-14 07:25:11 +1200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # arcgis-circadian-widget
 :sunrise: An ArcGIS JS V4 widget that changes the map tint based on the time of day.
+
+[![npm](https://img.shields.io/npm/v/arcgis-circadian-widget.svg?maxAge=2592000)](https://www.npmjs.com/package/arcgis-circadian-widget)
+
+Include the style, create the widget
+
+```js
+require([
+  "custom-widgets/Circadian"
+], function(Circadian) {
+
+  // create your map and view
+
+  var c = new Circadian();
+  c.startup();
+```
+
+Get it from here or via npm `npm install arcgis-circadian-widget --save`
+
+#### Running locally
+
+Download / clone this repository then run `npm install` > `npm start`
+
+![example](https://github.com/davetimmins/arcgis-circadian-widget/circadian.gif)
+
+
---
+++ src/Circadian.js
@@ -0,0 +1,70 @@
+
+define([  
+  "dojo/dom-class",
+
+  "esri/widgets/Widget"
+],
+function (
+  domClass,  
+  Widget
+) {
+
+  return Widget.createSubclass([],
+  {
+    declaredClass: "custom.widgets.Circadian",
+
+    postCreate: function () {
+      this.inherited(arguments);
+
+      this._tint = this._tint.bind(this);
+
+      // TODO : maybe allow this to be a property as may want to tint at container level
+      this._node = document.getElementsByClassName("esri-view-surface")[0];
+
+      if (!this._node) {
+        console.warn('No view surface found, unable to apply tinting.');
+      }
+    },
+
+    _node: null,
+
+    _timer: null,
+
+    _previousClass: null,
+
+    startup: function () {
+
+      if (!this._node) {
+        return;
+      }
+
+      domClass.add(this._node, "circadian-tint");
+            
+      this._tint();
+
+      // check the tint every minute
+      // TODO : could be smarter here and just check on the hour by calculating minutes left and using setTimeout
+      _timer = setInterval(this._tint, 1000 * 6);
+    },
+
+    destroy: function () {
+
+      clearTimeout(_timer);
+      _timer = null;
+    },
+
+    _tint: function () {
+
+      var hours = new Date().getHours();
+      var css = "tint-" + hours;
+      
+      if (this._previousClass && css !== this._previousClass) {
+
+        domClass.remove(this._node, this._previousClass);
+      }
+
+      domClass.add(this._node, "tint-" + hours);
+      this._previousClass = "tint-" + hours;
+    }
+  });
+});
